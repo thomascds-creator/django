c47c4e70e724a0b0f76e390accfd751836971721
thomascds-creator <kelenikelenichannel@gmail.com>
2022-05-31 06:34:14 +0300

    Update settings.py

--- revesztakumu/revesztakumu/settings.py
+++ revesztakumu/revesztakumu/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xo4!t48d#%i4h*40!tb1$2@2et+mf$m24*1v(-#xdy(m-*q@b#'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
